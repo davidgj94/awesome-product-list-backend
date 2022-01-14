9fe0e5d6f9a1662901e3a99c26c5b6270eb05003
David Gallardo <davidgj1994@gmail.com>
2022-01-14 09:45:16 +0100

    extended seeding script

--- tests/seed.ts
+++ tests/seed.ts
@@ -1,4 +1,4 @@
-import { Types } from "mongoose";
+import moment from "moment";
 
 import { initDatabase } from "../src/database";
 import productFactory from "./factories/product";
@@ -6,8 +6,35 @@ import orderFactory from "./factories/order";
 
 (async function () {
   await initDatabase(3);
-  const product = await productFactory();
+  const product1 = await productFactory();
+
+  const product2 = await productFactory();
+
+  const product3 = await productFactory();
+
+  const firstOrderDate = new Date();
+  const secondOrderDate = moment(firstOrderDate).add(1, "hour").toDate();
+  const thirdOrderDate = moment(secondOrderDate).add(1, "hour").toDate();
+
   await orderFactory({
-    products: [{ product: product._id, quantity: 2 }],
+    products: [
+      { product: product1._id, quantity: 5 },
+      { product: product2._id, quantity: 1 },
+      { product: product3._id, quantity: 6 },
+    ],
+    createdAt: firstOrderDate,
+  });
+
+  await orderFactory({
+    products: [
+      { product: product1._id, quantity: 5 },
+      { product: product2._id, quantity: 1 },
+    ],
+    createdAt: secondOrderDate,
+  });
+
+  await orderFactory({
+    products: [{ product: product2._id, quantity: 1 }],
+    createdAt: thirdOrderDate,
   });
 })();
